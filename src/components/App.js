de0c6e758d8d13ef95044dcc6cbc3c4bf3a25896
Diego Valles <dgvall134@gmail.com>
2022-11-29 15:45:03 -0700

    Created json file, replaced static data

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react"
+import React, {useState, useEffect} from "react"
 import {Switch, Route} from "react-router-dom"
 import NavBar from "./NavBar"
 import Game from "./Game"
@@ -9,37 +9,21 @@ import Leaderboard from "./Leaderboard"
 function App() {
   const [pets, setPets] = useState([])
 
-  // useEffect GET request to pets state
+  useEffect(() => {
+    fetch("http://localhost:3000/pets")
+    .then(res => res.json())
+    .then(data => setPets(data))
+  }, [])
 
-  const staticPets = [
-    {
-      id: 1,
-      name: "Jared",
-      image: "https://i.natgeofe.com/n/4f5aaece-3300-41a4-b2a8-ed2708a0a27c/domestic-dog_thumb_square.jpg",
-      type: "Dog"
-    },
-    {
-      id: 2,
-      name: "Max",
-      image: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/golden-retriever-royalty-free-image-506756303-1560962726.jpg?crop=0.672xw:1.00xh;0.166xw,0&resize=640:*",
-      type: "Dog"
-    },
-    // {
-        // id: 3,
-    //   name: "Kit",
-    //   image: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/cute-cat-photos-1593441022.jpg?crop=0.670xw:1.00xh;0.167xw,0&resize=640:*",
-    //   type: "Cat"
-    // }
-  ]
   
-  // const displayedPets = pets
+  const displayedPets = pets
 
   return (
     <div>
       <NavBar />
       <Switch>
         <Route exact path = "/">
-          <Game pets = {staticPets}/>
+          <Game pets = {displayedPets}/>
         </Route>
         <Route exact path = "/leaderboard">
           <Leaderboard />
